5f2ddd2201943b316396ea35915ec02b64485760
VladBermishev <vlad-bermishev@yandex.ru>
2023-01-13 20:41:58 +0300

    lab5 graphviz test

--- lab5/main.cpp
+++ lab5/main.cpp
@@ -3,7 +3,8 @@
 #include <iostream>
 #include <common/cmdline_positional_parser.h>
 #include <common/stream-extension.h>
-
+#include <gvc.h>
+#include <gvplugin.h>
 
 void setup_input_stream(std::ifstream& fin, istream_extension& in, const std::string& source) {
     if (source != "stdin") {
@@ -23,5 +24,20 @@ int main(int argc, const char* argv[]){
     istream_extension input(std::cin.rdbuf());
     std::ifstream finput;
     setup_input_stream(finput, input, parser[1].as<std::string>());
-    return 0;
+    Agraph_t *g;
+    Agnode_t *n, *m;
+    Agedge_t *e;
+    Agsym_t *a;
+    GVC_t *gvc;
+    gvc = gvContext();
+    g = agopen("g", Agdirected, nullptr);
+    n = agnode(g, "n", 1);
+    m = agnode(g, "m", 1);
+    e = agedge(g, n, m, 0, 1);
+    agsafeset(n, "color", "red", "");
+    gvLayout(gvc, g, "dot");
+    gvRenderFilename(gvc, g, "svg", "example.svg");
+    gvFreeLayout(gvc, g);
+    agclose(g);
+    return (gvFreeContext(gvc));
 }
